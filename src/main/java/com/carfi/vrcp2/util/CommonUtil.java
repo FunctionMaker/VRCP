95fe37dc84de526218d023e5834bb56c0ede0418
liutianxue <liutianxue@mail.local>
2016-12-30 14:08:21 +0800

    添加常用工具类CommonUtilã.java

---
+++ src/main/java/com/carfi/vrcp2/util/CommonUtil.java
@@ -0,0 +1,20 @@
+package com.carfi.vrcp2.util;
+
+import java.util.UUID;
+
+/**
+ * 公用工具类
+ * @author ltx
+ *
+ */
+public class CommonUtil {
+	
+	/**
+	 * 生成主键
+	 * @return
+	 */
+	public static String generatePrimaryKey(){
+		return UUID.randomUUID().toString().replace("-", "");
+	}
+
+}
