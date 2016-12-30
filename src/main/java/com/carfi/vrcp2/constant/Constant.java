e87933eadf3948dcc29a3f4b360d4500b04493f5
liutianxue <liutianxue@mail.local>
2016-12-30 16:48:55 +0800

    删除合并过程中产生的多余的vrcp2的包

--- src/main/java/com/carfi/vrcp2/constant/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.carfi.vrcp2.constant;
-
-public interface Constant {
-	String USER_IN_SESSION = "USER_IN_SESSION";
-	String VERIFY_CODE = "VERIFY_CODE";
-	String ORG_USER_IN_SESSION = "ORG_USER_IN_SESSION";
-	String CURRENT_LOGIN_TIME = "CURRENT_LOGIN_TIME";
-}
--- src/main/java/com/carfi/vrcp2/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.carfi.vrcp2.service;
-
-import com.carfi.vrcp2.pojo.User;
-
-public interface IUserService extends IBaseService<User>{
-	
-	User queryOneByName(String name);
-	
-}
